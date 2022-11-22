01acc75e0ca8f6b1b72f3a74bdd9a374e518716f
Murtaza Ali <murtaza5152.ali@gmail.com>
2022-11-22 13:07:02 -0800

    Tried to add a view but not working

--- django_project/topics/views.py
+++ django_project/topics/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import QuestionForm
 
 def home(request):
     return render(request, 'topics/home.html')
@@ -7,5 +8,22 @@ def home(request):
 def topic(request):
     return render(request, 'topics/topic.html', {'title': 'List of Topics'})
 
+# def problems(request):
+#     return render(request, 'topics/problems.html', {'title': 'List of Problems'})
+
 def problems(request):
-    return render(request, 'topics/problems.html', {'title': 'List of Problems'})
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = QuestionForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = QuestionForm()
+    return render(request, 'topics/problems.html', {'title': 'List of Problems', 'form': form})
