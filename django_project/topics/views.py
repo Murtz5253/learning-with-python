f013e8ccb5768bb8528102346adba5cd676964c2
Murtaza Ali <murtaza5152.ali@gmail.com>
2022-11-23 11:00:06 -0800

    Code editor showing on problems page woohoo!

--- django_project/topics/views.py
+++ django_project/topics/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import QuestionForm
+from .models import Question, QuestionForm
 
 def home(request):
     return render(request, 'topics/home.html')
@@ -12,18 +12,14 @@ def topic(request):
 #     return render(request, 'topics/problems.html', {'title': 'List of Problems'})
 
 def problems(request):
-    # if this is a POST request we need to process the form data
     if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = QuestionForm(request.POST)
-        # check whether it's valid:
+        form = QuestionForm(request.POST, initial={'question_code': '# YOUR CODE HERE'})
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('/thanks/')
-
-    # if a GET (or any other method) we'll create a blank form
+            form.save()
+            return redirect('/')
     else:
-        form = QuestionForm()
-    return render(request, 'topics/problems.html', {'title': 'List of Problems', 'form': form})
+        form = QuestionForm(initial={'question_code': '# YOUR CODE HERE'})
+    return render(request, "topics/problems.html", {
+        "form": form,
+        "problems": Question.objects.all()
+    })
