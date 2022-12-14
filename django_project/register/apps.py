9c17aa5a472e25a4877adef71d4bd604a8d4d799
Carolyn Clark <clarkc9@uw.edu>
2022-12-13 16:20:44 -0800

    Django_project is PEP8 Compliant. Run pylint django_project/ --rcfile=.pylintrc before committing to verify any additions are PEP8 Compliant.

--- django_project/register/apps.py
+++ django_project/register/apps.py
@@ -1,6 +1,8 @@
+"""This module configures the register form"""
 from django.apps import AppConfig
 
 
 class RegisterConfig(AppConfig):
+    """Sets registration configuration to autofield and name register"""
     default_auto_field = "django.db.models.BigAutoField"
     name = "register"
